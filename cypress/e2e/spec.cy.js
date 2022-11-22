619e69d75af5469bd5fd348eb13d46db46291c0f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-22 10:09:46 -0500

    add h2 placeholder

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -19,3 +19,21 @@ it('closes the cookie banner', () => {
   // and it should do nothing if there is no banner
   closeTheCookieBanner()
 })
+
+it('removes the cookie if it exists to force the banner to appear', () => {
+  // uncomment to set the cookie
+  // and force the "IF(exist)" commands path
+  // cy.visit('index.html')
+  // cy.contains('button', 'Agree').click()
+  //
+  // get the cookie named "cookieConsent"
+  // and if it exists, clear it
+  // https://on.cypress.io/getcookie
+  // https://on.cypress.io/clearcookie
+  //
+  // if the cookie does not exist
+  // log "No cookie" to the Command Log
+  //
+  // visit the "index.html" and confirm
+  // the cookie consent banner is visible
+})
