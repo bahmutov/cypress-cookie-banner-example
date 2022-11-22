c1552738e041358b4a7dbb54537f5b25f9d724cc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-22 10:09:21 -0500

    add another test placeholder

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -29,3 +29,21 @@ it('once closed it stays hidden', () => {
   cy.reload()
   cy.get('aside.banner').should('not.be.visible')
 })
+
+it('removes the cookie if it exists to force the banner to appear', () => {
+  // uncomment to set the cookie
+  // and force the "IF(exist)" commands path
+  // cy.visit('index.html')
+  // cy.contains('button', 'Agree').click()
+  //
+  // get the cookie named "cookieConsent"
+  // and if it exists, clear it
+  // https://on.cypress.io/getcookie
+  // https://on.cypress.io/clearcookie
+  //
+  // if the cookie does not exist
+  // log "No cookie" to the Command Log
+  //
+  // visit the "index.html" and confirm
+  // the cookie consent banner is visible
+})
