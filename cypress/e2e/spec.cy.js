fe7a67204efd5df6776d2fe8231a6401dbe158ab
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-22 09:14:47 -0500

    the initial exercise

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,30 +2,20 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
 
+function closeTheCookieBanner() {
+  // check if the banner is visible
+  // if yes, click the "Agree" button
+  // else do nothing
+  // confirm the banner is not visible
+}
+
 it('closes the cookie banner', () => {
   cy.visit('index.html')
-  cy.get('aside.banner').should('be.visible')
-  cy.contains('button', 'Agree').click()
-  cy.get('aside.banner').should('not.be.visible')
-  // confirm the cookie consent was set
-  // Tip: inspect the application tab in the DevTools
-  cy.getCookie('cookieConsent').should('deep.include', {
-    name: 'cookieConsent',
-    value: 'given',
-  })
-})
-
-it('prevents the cookie banner from showing up', () => {
-  cy.setCookie('cookieConsent', 'given')
-  cy.visit('index.html')
-  cy.get('aside.banner').should('not.be.visible')
-})
-
-it('once closed it stays hidden', () => {
-  cy.visit('index.html')
-  cy.get('aside.banner').should('be.visible')
-  cy.contains('button', 'Agree').click()
-  cy.get('aside.banner').should('not.be.visible')
+  closeTheCookieBanner()
+  // confirm the banner stays closed
+  // by reloading the page
   cy.reload()
-  cy.get('aside.banner').should('not.be.visible')
+  // but we still can call the utility function
+  // and it should do nothing if there is no banner
+  closeTheCookieBanner()
 })
