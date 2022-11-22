810806882f4151a78a73586f8f0d3229ead2a331
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-22 08:55:26 -0500

    initial

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,16 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="cypress" />
+
+it('closes the cookie banner', () => {
+  cy.visit('index.html')
+  cy.get('aside.banner').should('be.visible')
+  cy.contains('button', 'Agree').click()
+  cy.get('aside.banner').should('not.be.visible')
+  // confirm the cookie consent was set
+  // Tip: inspect the application tab in the DevTools
+  cy.getCookie('cookieConsent').should('deep.include', {
+    name: 'cookieConsent',
+    value: 'given',
+  })
+})
